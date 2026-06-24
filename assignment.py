f91a825bad340468460bcecd6c6b10d13f74bd8f
jkumarvardhan-ux <jkumarvardhan@gmail.com>
2026-06-24 11:35:34 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,25 @@
+# Assignment Operators Example
+
+a = 10
+print("Initial value of a =", a)
+
+a += 5      # a = a + 5
+print("After += 5 :", a)
+
+a -= 3      # a = a - 3
+print("After -= 3 :", a)
+
+a *= 2      # a = a * 2
+print("After *= 2 :", a)
+
+a /= 4      # a = a / 4
+print("After /= 4 :", a)
+
+a //= 2     # a = a // 2
+print("After //= 2 :", a)
+
+a %= 3      # a = a % 3
+print("After %= 3 :", a)
+
+a **= 2     # a = a ** 2
+print("After **= 2 :", a)
