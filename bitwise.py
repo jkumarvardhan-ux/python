d050506ec7aee7b4e555e87ea905250f172ba44f
jkumarvardhan-ux <jkumarvardhan@gmail.com>
2026-06-24 11:51:39 +0530

    Add files via upload

---
+++ bitwise.py
@@ -0,0 +1,11 @@
+# Bitwise Operators Example
+
+a = 10   # Binary: 1010
+b = 4    # Binary: 0100
+
+print("a & b =", a & b)    # AND
+print("a | b =", a | b)    # OR
+print("a ^ b =", a ^ b)    # XOR
+print("~a =", ~a)          # NOT
+print("a << 1 =", a << 1)  # Left Shift
+print("a >> 1 =", a >> 1)  # Right Shift
