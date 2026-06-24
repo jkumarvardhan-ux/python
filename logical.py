060d9485b3ed5466ec76c1df116e1292fcdb3fcd
jkumarvardhan-ux <jkumarvardhan@gmail.com>
2026-06-24 11:40:23 +0530

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,14 @@
+# Logical Operators Example
+
+a = 10
+b = 20
+c = 30
+
+# AND Operator
+print("a < b and b < c :", a < b and b < c)
+
+# OR Operator
+print("a > b or b < c :", a > b or b < c)
+
+# NOT Operator
+print("not(a < b) :", not(a < b))
